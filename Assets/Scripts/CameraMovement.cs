c6ff1faef92ae47846724af7c64ea2fac906c00f
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-01-31 21:52:26 -0700

    Add files via upload

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float mouseInfluenceAmount = 1f;
+    //The adjusted mouse position from the center of the screen
+    private Vector3 mouseAdjust;
+    //The vector to lerp to
+    private Vector3 mouseDirection;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //Reference mouse position, adjusted for the player being in the center of the screen
+        mouseAdjust = new Vector3(Input.mousePosition.x - (Screen.width / 2), Input.mousePosition.y - (Screen.height / 2), player.transform.position.z);
+        //Get the direction for camera offset
+        mouseDirection = player.transform.position - mouseAdjust;
+        mouseDirection = mouseDirection.normalized;
+        mouseDirection *= mouseInfluenceAmount;
+        //Lerp smoothly to new player position, informed by mouse offset
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x - mouseDirection.x, player.transform.position.y - mouseDirection.y, transform.position.z), 0.25f);
+    }
+}
---
+++ Assets/Scripts/UI_Energy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Energy: MonoBehaviour
+{
+    public Slider energySlider;
+
+    public void SetEnergy(float newEnergy)
+    {
+        energySlider.value = newEnergy;
+    }
+
+}
